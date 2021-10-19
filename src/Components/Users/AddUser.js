febc82f653945ab2c59fe210e1f521c6bca0da9e
Paul Mackay <paul.m.mackay85@gmail.com>
2021-10-19 15:41:04 +0100

    Controlled vs Uncontrolled Components

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -20,7 +20,7 @@ const AddUser = (props) => {
     if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
       setError({
         title: 'Invalid input',
-        message: 'Please enter a valid name and age (no empty values).',
+        message: 'Please enter a valid name and age (no empty fields).',
       });
       return;
     }
